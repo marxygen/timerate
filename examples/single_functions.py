f3d8b51479cbe668140aecb106973880b57c710c
marxygen@gmail.com <marxygen@gmail.com>
2020-12-11 22:41:14 +0300

    New example added

--- examples/single_functions.py
+++ examples/single_functions.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append(r'C:\\Users\\Asus\\timerate\\') 
 
-from timerate.static import timed
+from timerate.static import timed, avgtime
 
 @timed
 def example1(N):
@@ -13,5 +13,10 @@ def example1(N):
 
     return fibs
 
+@avgtime(100)
+def example2(N):
+    [x for x in range(N)]
+
 if __name__ == '__main__':
     print(example1(5))
+    print(example2(1000))
